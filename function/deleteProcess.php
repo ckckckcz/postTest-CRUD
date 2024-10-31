21515ee8e76b2307c17a2bf04cd6a66185b50eef
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-31 20:16:29 +0700

    remove unecessary comment between php code

--- function/deleteProcess.php
+++ function/deleteProcess.php
@@ -1,24 +1,17 @@
 <?php
-// Mengimpor kode program yang ada di database.php
-include '../routes/database.php'; // Periksa kembali jalur ini
+include '../routes/database.php';
 
-// Memeriksa apakah ada data bernama 'id'
 if (isset($_POST['id'])) {
-    // Mengambil value bersih dari $_POST ke dalam variabel
-    $id = htmlspecialchars($_POST['id']); // Mengambil ID yang dikirim dari formulir
+    $id = htmlspecialchars($_POST['id']);
 
-    // Query SQL untuk menghapus data
     $query = "DELETE FROM todos WHERE id = ?";
-    // Parameter atau data yang akan dimasukkan ke dalam tanda tanya (?) di query
     $params = [$id];
 
-    // Eksekusi query
     $sql = sqlsrv_query($conn, $query, $params);
 
-    // Jika query berhasil, kembali ke halaman index
     if ($sql) {
         header("Location: ../index.php");
-        exit(); // Pastikan untuk menghentikan eksekusi script setelah redirect
+        exit();
     } else {
         $errors = print_r(sqlsrv_errors(), true);
         echo "<script>alert('$errors');</script>";
--- function/readProcess.php
+++ function/readProcess.php
@@ -1,22 +1,18 @@
 <?php
-include './routes/database.php'; // Menghubungkan ke file database Anda
+include './routes/database.php';
 
-// Memeriksa apakah ID ada dalam URL
 if (isset($_GET['id'])) {
-    $id = intval($_GET['id']); // Mengambil ID dari URL dan mengubahnya menjadi integer
+    $id = intval($_GET['id']); 
 
-    // Query untuk mengambil data berdasarkan ID
-    $query = "SELECT * FROM todos WHERE id = ?"; // Menggunakan placeholder
-    $params = array($id); // Parameter untuk query
+    $query = "SELECT * FROM todos WHERE id = ?";
+    $params = array($id);
 
-    $stmt = sqlsrv_query($conn, $query, $params); // Eksekusi query
+    $stmt = sqlsrv_query($conn, $query, $params);
 
-    // Memeriksa apakah data ditemukan
     if ($stmt === false) {
-        die(print_r(sqlsrv_errors(), true)); // Menampilkan error jika query gagal
+        die(print_r(sqlsrv_errors(), true)); 
     }
 
-    // Mengambil data
     if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     } else {
         echo "Tugas tidak ditemukan.";

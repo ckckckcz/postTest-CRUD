71a048271606664895a8fe381d93f01a60e4dac3
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-31 20:12:16 +0700

    refactor: change file name from create.php to tambah_tugas.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($conn != false) {
             <p class="text-md font-normal text-gray-500 lg:text-lg dark:text-gray-400">Kami ada untuk membantu Anda
                 menjalani hari-hari yang lebih teratur dan bermakna.</p>
             <?php if ($isDbConnectionSuccess) { ?>
-                <a href="create.php">
+                <a href="tambah_tugas.php">
                     <button type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-10 flex gap-2">
                         <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"

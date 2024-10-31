53b337a7437a3ab992ec1b4795c63689e451a265
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-31 20:17:57 +0700

    refactor: change file name from read.php to lihat_detail.php

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ if ($conn != false) {
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                 <?php echo htmlspecialchars($row['deskripsi']); ?>
                             </p>
-                            <a href="read.php?id=<?php echo $row['id']; ?>"
+                            <a href="lihat_detail.php?id=<?php echo $row['id']; ?>"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Lihat Tugas
                             </a>

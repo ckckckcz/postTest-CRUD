c8e364c9b82341f8a44dcfa295d3cd403373f300
ckckckcz <rriovld@gmail.com>
2024-10-31 13:21:43 +0700

    update: function create done

--- create.php
+++ create.php
@@ -1,6 +1,26 @@
 <?php
-include './routes/conn.php';
+include './routes/database.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $judul = $_POST['judul'];
+    $deskripsi = $_POST['deskripsi'];
+
+    $sql = "INSERT INTO todos (judul, deskripsi) VALUES (?, ?)";
+    $params = array($judul, $deskripsi);
+
+    $stmt = sqlsrv_query($conn, $sql, $params);
+
+    if ($stmt) {
+        echo "Data berhasil disimpan.";
+    } else {
+        echo "Terjadi kesalahan: " . print_r(sqlsrv_errors(), true);
+    }
+
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,64 +41,19 @@ include './routes/conn.php';
             <p class="text-md font-normal text-gray-500 lg:text-lg dark:text-gray-400">Setiap tugas yang Anda tulis
                 adalah langkah menuju kesuksesan!</p>
             <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-            <form action="" class="text-lg">
+            <form action="create.php" method="POST" class="text-lg">
                 <div class="mb-5">
-                    <label for="judul" class="block mb-2  font-medium text-gray-900 dark:text-white">Judul</label>
-                    <input type="text" id="judul" placeholder="Masukkan Judul"
-                        class="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required />
+                    <label for="judul" class="block mb-2 font-medium text-gray-900 dark:text-white">Judul</label>
+                    <input type="text" id="judul" name="judul" placeholder="Masukkan Judul" required
+                        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
                 <div class="mb-5">
                     <label for="deskripsi"
-                        class="block mb-2  font-medium text-gray-900 dark:text-white">Deskripsi</label>
-                    <textarea id="message" rows="4"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Masukkan Keterangan"></textarea>
+                        class="block mb-2 font-medium text-gray-900 dark:text-white">Deskripsi</label>
+                    <textarea id="deskripsi" name="deskripsi" rows="4" placeholder="Masukkan Keterangan" required
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
                 </div>
-                <div class="tanggalPicker grid grid-cols-2 gap-5 mb-5 lg:grid lg:grid-cols-4">
-                    <div class="relative w-full ">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
-                        </div>
-                        <input datepicker id="default-datepicker-mulai" type="text"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Tanggal Mulai">
-                    </div>
-                    <div class="relative w-full ">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path
-                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
-                        </div>
-                        <input datepicker id="default-datepicker-selesai" type="text"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Tanggal Selesai">
-                    </div>
-                    <div class="w-full kategori">
-                        <select id="kategori"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option>Kategori</option>
-                            <option>Penting</option>
-                            <option>Santai</option>
-                        </select>
-                    </div>
-                    <div class="w-full warnaBorder">
-                        <select id="warnaBorder"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option>Pilih Warna</option>
-                            <option>Merah</option>
-                            <option>Biru</option>
-                            <option>Hijau</option>
-                        </select>
-                    </div>
-                </div>
-                <button type="button"
+                <button type="submit"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Buat
                     Tugas</button>
             </form>
---
+++ routes/database.php
@@ -0,0 +1,16 @@
+<?php
+
+$serverName = "RIOVALDOALFIYAN\MSQLRIO";
+$connectionInfo = [
+    "Database" => "crud"
+];
+
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if ($conn) {
+    echo "Koneksi ke database berhasil!";
+} else {
+    echo "Koneksi ke database gagal: ";
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
